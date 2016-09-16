fa2144f33bf4c4d110bde341493c131b5981b1ce
tiagommferreira <tiagommferreira55@gmail.com>
2016-09-16 16:35:50 +0100

    added navbar

--- client/components/Layout.js
+++ client/components/Layout.js
@@ -1,23 +1,39 @@
 import React from "react"
-import { connect } from "react-redux"
 
-import { fetchUser } from "../actions/userActions"
+import Nav from "./Nav"
 
+//import { connect } from "react-redux"
+
+//import { fetchUser } from "../actions/userActions"
+
+/*
 @connect((store) => {
   return {
     user: store.user.user,
     userFetched: store.user.fetched,
   };
 })
-export default class Layout extends React.Component {
-  componentWillMount() {
-    this.props.dispatch(fetchUser())
-  }
+*/
 
+export default class Layout extends React.Component {
   render() {
+    const { location } = this.props;
+    const containerStyle = {
+      marginTop: "60px"
+    };
+
     return (
         <div>
-            {this.props.children}
+
+          <Nav location={location} />
+          <div class="container" style={containerStyle}>
+            <div class="row">
+              <div class="col-lg-12">
+                {this.props.children}
+              </div>
+            </div>
+          </div>
+
         </div>
     );
   }
